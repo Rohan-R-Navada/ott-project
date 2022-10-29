599e1971e3a11d05ba5f9c969c711b808d650ad3
Rohan R Navada <rohannav07@gmail.com>
2022-10-29 12:52:57 +0530

    changes 12.50pm

--- filter.js
+++ filter.js
@@ -1,47 +1,24 @@
-filterSelection("all")
-function filterSelection(c) {
-  var x, i;
-  x = document.getElementsByClassName("movies");
-  if (c == "all") c = "";
-  // Add the "show" class (display:block) to the filtered elements, and remove the "show" class from the elements that are not selected
-  for (i = 0; i < x.length; i++) {
-    w3RemoveClass(x[i], "show");
-    if (x[i].className.indexOf(c) > -1) w3AddClass(x[i], "show");
-  }
-}
+// const btn = document.querySelectorAll('.btn');
+// const allbtn = document.querySelectorAll('.movies')
 
-// Show filtered elements
-function w3AddClass(element, name) {
-  var i, arr1, arr2;
-  arr1 = element.className.split(" ");
-  arr2 = name.split(" ");
-  for (i = 0; i < arr2.length; i++) {
-    if (arr1.indexOf(arr2[i]) == -1) {
-      element.className += " " + arr2[i];
-    }
-  }
-}
+// for(let i = 0; i < btn.length; i++){
+//   btn[i].addEventListener('click', filterPosts.bind(this, btn[i]));
+// }
 
-// Hide elements that are not selected
-function w3RemoveClass(element, name) {
-  var i, arr1, arr2;
-  arr1 = element.className.split(" ");
-  arr2 = name.split(" ");
-  for (i = 0; i < arr2.length; i++) {
-    while (arr1.indexOf(arr2[i]) > -1) {
-      arr1.splice(arr1.indexOf(arr2[i]), 1);
-    }
-  }
-  element.className = arr1.join(" ");
-}
+// function filterPosts(item){
+//   changeActivePosition(item);
+//   for(let i = 0; i < allbtn.length; i++){
+//       if(allbtn[i].classList.contains(item.attributes.id.value)){
+//           allbtn[i].style.display = "block";
+//       } else {
+//           allbtn[i].style.display = "none";
+//       }
+//   }
+// }
 
-// Add active class to the current control button (highlight it)
-var btnContainer = document.getElementById("myBtnContainer");
-var btns = btnContainer.getElementsByClassName("btn");
-for (var i = 0; i < btns.length; i++) {
-  btns[i].addEventListener("click", function() {
-    var current = document.getElementsByClassName("active");
-    current[0].className = current[0].className.replace(" active", "");
-    this.className += " active";
-  });
-}
+// function changeActivePosition(activeItem){
+//   for(let i = 0; i < btn.length; i++){
+//       btn[i].classList.remove('active');
+//   }
+//   activeItem.classList.add('active');
+// };
