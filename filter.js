f69c27a19e2c7348ddaa8302e6281b05ca3c843f
Rohan R Navada <rohannav07@gmail.com>
2022-10-30 10:44:24 +0530

    sunday 10.45

--- filter.js
+++ filter.js
@@ -22,3 +22,24 @@
 //   }
 //   activeItem.classList.add('active');
 // };
+
+// function myFunction() {
+//     // Declare variables
+//     var input, filter, ul, li, a, i, txtValue;
+//     input = document.getElementById('myInput');
+//     filter = input.value.toUpperCase();
+//     ul = document.getElementById("container");
+//    li=ul.doStuff(document.querySelectorAll("#kids, #horror, #comedy, #romance,#murder ,#fantasy"));
+  
+//     // Loop through all list items, and hide those who don't match the search query
+//     for (i = 0; i < li.length; i++) {
+//       a = li[i].doStuff(document.querySelectorAll("#kids, #horror, #comedy, #romance,#murder ,#fantasy"));
+//       [0];
+//       txtValue = a.textContent || a.innerText;
+//       if (txtValue.toUpperCase().indexOf(filter) > -1) {
+//         li[i].style.display = "";
+//       } else {
+//         li[i].style.display = "none";
+//       }
+//     }
+//   }
